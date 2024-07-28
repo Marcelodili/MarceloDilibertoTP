3946b2b09731c6a893a08f60811721c7d07883a5
mdiliberto <mdiliberto@meta.com.ar>
2024-07-28 13:04:43 -0300

    7

--- MD/App01/urls.py
+++ MD/App01/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from App01 import views
+from App01.views import inicio
+from App01.views import busqueda
+from App01.views import estado
+from App01.views import ingrese
+from App01.views import ofertas
 from MD.view import msg01
 from MD.view import hoy
 from MD.view import param01
 from MD.view import Template01
+
 
 urlpatterns = [
     # inicio
@@ -12,5 +18,10 @@ urlpatterns = [
     path('MD02/', hoy),
     path('MD03/<parametro01>', param01),
     path('MD04/', Template01),
+    path("inicio", inicio),
+    path("busqueda", busqueda),
+    path("estado", estado),
+    path("ingrese", ingrese),
+    path("ofertas", ofertas),
 ]
 
--- MD/App01/views.py
+++ MD/App01/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import path, include
+
 
 # Create your views here.
+def inicio(request):
+
+    return render(request, "App01/inicio.html")
+
+
+def busqueda(request):
+
+    return render(request, "App01/busqueda.html")
+
+
+def estado(request):
+
+    return render(request, "App01/estado.html")
+
+
+def ingrese(request):
+
+    return render(request, "App01/ingrese.html")
+
+
+def ofertas(request):
+
+    return render(request, "App01/ofertas.html")
