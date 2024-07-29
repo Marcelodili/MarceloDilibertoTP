b4212b5a0313c5f92595b37edd08ace22bfa987a
mdiliberto <mdiliberto@meta.com.ar>
2024-07-28 23:04:21 -0300

    11 alta db ok

--- MD/App01/views.py
+++ MD/App01/views.py
@@ -24,27 +24,27 @@ def estado(request):
 
     return render(request, "App01/estado.html")
 
-
-def ingrese(request):
-
-    return render(request, "App01/ingrese.html")
-
-
 def ofertas(request):
     ofertas = Destino.objects.all() 
     contexto = {"ofertas": ofertas} 
     return render(request, "App01/ofertas.html", contexto)
 
 
-def formIngrese(request):
+def ingrese(request):
     print(request.POST)
     if request.method == "POST":
-        mi_formulario = IngresarFormulario(request.POST)
-        if mi_formulario.is_valid():
-            informacion = mi_formulario.cleaned_data
-            ingrese = Presupuestar(clientedni=informacion["clientedni"], codigotransporte=informacion["codigotransporte"], codigodestino=informacion["codigodestino"], comentario=informacion["comentario"], fechaviaje=informacion["fechaviaje"], dias=informacion["dias"], pasajeros=informacion["pasajeros"])
-            ingrese.save()
+        print(request.POST)
+#        mi_formulario = IngresarFormulario(request.POST)
+#        if mi_formulario.is_valid():
+#            informacion = mi_formulario.cleaned_data
+#            ingrese = Presupuestar(clientedni=informacion["clientedni"], codigotransporte=informacion["codigotransporte"], codigodestino=informacion["codigodestino"], comentario=informacion["comentario"], fechaviaje=informacion["fechaviaje"], dias=informacion["dias"], pasajeros=informacion["pasajeros"])
+#            ingrese.save()
+#        return render(request, "App01/inicio.html")
+#    else:
+#        mi_formulario = IngresarFormulario()
+#    return render(request, "App01/ingrese.html", {"mi_formulario": mi_formulario})
+        ingrese = Presupuestar(clientedni=request.POST["clientedni"], codigotransporte=request.POST["codigotransporte"], codigodestino=request.POST["codigodestino"], comentario=request.POST["comentario"], fechaviaje=request.POST["fechaviaje"], dias=request.POST["dias"], pasajeros=request.POST["pasajeros"])
+        print(request.POST)
+        ingrese.save()
         return render(request, "App01/inicio.html")
-    else:
-        mi_formulario = IngresarFormulario()
-    return render(request, "App01/ingrese.html", {"mi_formulario": mi_formulario})
+    return render(request, "App01/ingrese.html")
